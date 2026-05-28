67319801aeb443f3e1d0f68c8af97862633d46b9
Gustav Berloty <gustavberloty2@gmail.com>
2026-05-28 09:00:35 +0200

    -

--- sources/MARTIN/fixing_versification/step2/step2-3.sh
+++ sources/MARTIN/fixing_versification/step2/step2-3.sh
@@ -1,8 +1,23 @@
-#!/bin/sh
+#!/nix/store/lfbzxs5wyqd2122mpbj5azkxhxspw9cd-bash-interactive-5.3p3/bin/bash
+# $1 : fichier avec les bbcccvvv cibles
+# $2 : fichier à nettoyer
+
+declare -A targeted_ids
 while IFS= read -r line; do
-  ccc="${line:2:3}"
-  vvv="${line:5:3}"
-  chapter=$((10#$ccc))
-  verse=$((10#$vvv))
-  echo "$line" | sed "s/ *($chapter:$verse) */ /g; s/  */ /g"
+    id="${line:0:8}"
+    targeted_ids["$id"]=1
 done < "$1"
+
+while IFS= read -r line; do
+    id="${line:0:8}"
+    if [[ -n "${targeted_ids[$id]}" ]]; then
+        ccc="${id:2:3}"
+        vvv="${id:5:3}"
+        c=$((10#$ccc))
+        v=$((10#$vvv))
+        if [[ "$line" =~ "^${id} *\($c:$v\)" ]]; then
+            line=$(echo "$line" | sed "s/ *($c:$v) */ /g; s/  */ /g")
+        fi
+    fi
+    echo "$line"
+done < "$2"
